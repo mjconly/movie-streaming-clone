b9f1bcdb9bde33feee5aabb41eb696f933e48e17
Mike <mjconly@gmail.com>
2020-03-25 04:51:39 -0600

    added post review routes

--- models/Review.js
+++ models/Review.js
@@ -1,7 +1,15 @@
 const mongoose = require("mongoose");
 
 const ReviewSchema = new mongoose.Schema({
-  posted_by: String,
+  posted_by_name: String,
+  posted_by_id: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Review"
+  }],
+  posted_for:[{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Movie"
+  }],
   posted_on: {
     type: Date,
     default: Date.now
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 const Movie = require("../models/Movie");
 const Actor = require("../models/Actor");
+const Review = require("../models/Review");
 
 router.get("/:id", auth, (req, res) => {
 
@@ -24,5 +25,53 @@ router.get("/:id", auth, (req, res) => {
 })
 
 
-router.get("/:id")
+router.post("/review", auth, (req, res) => {
+  const posted_by_name = req.user.name;
+  const posted_by_id = req.body.userId;
+  const posted_for = req.body.movieId;
+  const score = req.body.rating;
+  const description = req.body.review;
+
+  let newReview = Review({
+    posted_by_name,
+    posted_by_id,
+    posted_for,
+    score,
+    description
+  })
+
+  newReview.save()
+    .then((review) => {
+      Movie.findOneAndUpdate({_id: posted_for},
+        {$push: {reviews: review._id}},
+        {new: true, useFindAndModify: false}
+      )
+        .then((movie) => {
+          Review.find({"_id": { $in: movie.reviews} })
+            .then((reviews) => {
+              res.json({
+                reviews
+              })
+            })
+        })
+        .catch((err) => {res.json(err)})
+    })
+    .catch((err) => {res.json(err)})
+
+})
+
+router.get("/:id/reviews", auth, (req, res) => {
+  Movie.findById(req.params.id)
+    .then((movie) => {
+      Review.find({"_id": {$in: movie.reviews} })
+        .then((reviews) => {
+          res.json({
+            reviews
+          })
+        })
+        .catch((err) => console.log(err))
+    })
+    .catch((err) => console.log(err))
+})
+
 module.exports = router;
